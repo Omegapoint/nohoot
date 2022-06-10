30aa0469f0c3f1ac64b309c7c3c65438777a6576
John Valinger <john.valinger@omegapoint.se>
2022-06-10 16:12:26 +0200

    Session implemented

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,58 @@
 #[macro_use] extern crate rocket;
 
-use rocket::serde::{json::Json, Deserialize, Serialize};   
+use rocket::{serde::{json::Json, Deserialize, Serialize} };   
+use uuid::Uuid;
 #[cfg(test)] mod test;
 
 
 #[derive(FromForm, Deserialize, Serialize)]
 struct Name {
-    name: String,
+    game_host: String,
+}
+#[derive(Serialize, Deserialize, Clone)]
+struct Answers {
+    text: String,
+    is_correct: bool,
+}
+#[derive(Serialize, Deserialize, Clone)]
+struct Question {
+    text: String,
+    answers: Vec<Answers>,
+}
+#[derive(Serialize, Deserialize, Clone)]
+struct Quiz {
+    title: String,
+    questions: Vec<Question>,
+}
+#[derive(Serialize, Deserialize)]
+struct Session {
+    id: Uuid,
+    game_host: String,
+    quiz: Quiz
 }
 
 pub static BASEURL:&str = "/v1";
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!\n"
-}
-
-#[get("/hello/<name>")]
-fn hello(name: String) -> String {
-    format!("Hello, {}!\n", name)
-}
-
-#[post("/session", format="application/json", data="<name>")]
-fn start_session(name: Json<Name>) -> String {
-    let r = format!("You get a session for {}!\n", name.name.as_str());
-    return r;
+#[post("/session", format="application/json", data="<game_host>")]
+fn start_session(game_host: Json<Name>) -> Json<Session> {
+    let first_answer = Answers { text: "No".to_string(), is_correct: true };
+    let second_answer = Answers { text: "Yes".to_string(), is_correct: false };
+    let a_question = Question { text: "Is rust the easiest language?".to_string(), answers: vec![first_answer, second_answer] };
+    let base_quiz: Quiz = Quiz {
+        title: "Best quiz!".to_string(),
+        questions: vec![
+            a_question
+            ],
+    };
+    let new_session = Session { id: (Uuid::new_v4()), game_host: (game_host.game_host.clone()), quiz: base_quiz.clone() };
+    return Json(new_session)
 }
 
 #[launch]
 pub fn start() -> _ {
-    rocket::build().mount(BASEURL, routes![index, start_session, hello])
+    let figment = rocket::Config::figment()
+        .merge(("port", 8080))
+        .merge(("address", "0.0.0.0"));
+
+    rocket::custom(figment).mount(BASEURL, routes![start_session])
 }
--- src/test.rs
+++ src/test.rs
@@ -2,7 +2,7 @@
 mod test {
     use rocket::{local::blocking::Client, http::{Status}};
 
-    use crate::{Name};
+    use crate::{Name, Session};
 
     #[test]
     fn hello_world() {
@@ -17,18 +17,19 @@ mod test {
     fn hello_name() {
         let rocket = crate::start();
         let client = Client::tracked(rocket).unwrap();
-        let response = client.get("/v1/hello/kalle").dispatch();
+        let response = client.get("/v1/hello/anka").dispatch();
         assert_eq!(response.status(), Status::Ok);
-        assert_eq!(response.into_string().unwrap(), "Hello, kalle!\n");
+        assert_eq!(response.into_string().unwrap(), "Hello, anka!\n");
     }
     
     #[test]
     fn session() {
         let rocket = crate::start();
         let client = Client::tracked(rocket).unwrap();
-        let a_name = Name { name: "John".to_string() };
+        let a_name = Name { game_host: "John".to_string() };
         let response = client.post("/v1/session").json(&a_name).dispatch();
         assert_eq!(response.status(), Status::Ok);
-        assert_eq!(response.into_string().unwrap(), format!("You get a session for {}!\n", a_name.name.to_string()));
+        let session = response.into_json::<Session>();
+        assert_eq!(session.unwrap().game_host, a_name.game_host);
     }
 }
